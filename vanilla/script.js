f7ad4a7ce86ce4a3f1e84f62821b1b6ece1661fa
diptobiswasanime4 <diptobiswasanime4@gmail.com>
2023-08-06 16:16:31 +0530

    refactor - Todo changes

--- vanilla/script.js
+++ vanilla/script.js
@@ -4,31 +4,35 @@ const todoContainer = document.querySelector(".container");
 
 addTodo.addEventListener("click", (e) => {
   if (inputTodo.value != "") {
-    let todo = document.createElement("div");
-    todo.classList.add("todo");
-
-    let todoText = document.createElement("p");
-    todoText.innerText = inputTodo.value;
-
-    let todoActions = document.createElement("div");
-    todoActions.classList.add("actions");
-
-    let checkbox = document.createElement("input");
-    checkbox.setAttribute("type", "checkbox");
-    let delBtn = document.createElement("button");
-    delBtn.innerText = "Delete";
-
-    delBtn.addEventListener("click", () => {
-      todoContainer.removeChild(todo);
-    });
-
-    todoActions.appendChild(checkbox);
-    todoActions.appendChild(delBtn);
-
-    todo.appendChild(todoText);
-    todo.appendChild(todoActions);
-
-    todoContainer.appendChild(todo);
+    displayTodo(inputTodo.value);
     inputTodo.value = "";
   }
 });
+
+function displayTodo(todoValue) {
+  let todo = document.createElement("div");
+  todo.classList.add("todo");
+
+  let todoText = document.createElement("p");
+  todoText.innerText = todoValue;
+
+  let todoActions = document.createElement("div");
+  todoActions.classList.add("actions");
+
+  let checkbox = document.createElement("input");
+  checkbox.setAttribute("type", "checkbox");
+  let delBtn = document.createElement("button");
+  delBtn.innerText = "Delete";
+
+  delBtn.addEventListener("click", () => {
+    todoContainer.removeChild(todo);
+  });
+
+  todoActions.appendChild(checkbox);
+  todoActions.appendChild(delBtn);
+
+  todo.appendChild(todoText);
+  todo.appendChild(todoActions);
+
+  todoContainer.appendChild(todo);
+}
