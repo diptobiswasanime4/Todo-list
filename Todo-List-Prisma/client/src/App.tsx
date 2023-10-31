84e85ab250cfcfa002827d913addb534667977b9
diptobiswasanime4 <diptobiswasanime4@gmail.com>
2023-10-31 09:47:50 +0530

    📦 setting up Primsa project

---
+++ Todo-List-Prisma/client/src/App.tsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <div className="">
+      <div className="text-2xl">Todo List Prisma</div>
+    </div>
+  );
+}
+
+export default App;
