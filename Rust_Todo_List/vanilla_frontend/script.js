2c67d518345c16ea562c95a278b47af4c16909bd
Dipto Biswas <diptobiswasanime4@gmail.com>
2024-02-08 00:02:04 +0530

    vanilla JS client

--- Rust_Todo_List/vanilla_frontend/script.js
+++ Rust_Todo_List/vanilla_frontend/script.js
@@ -1,55 +1,79 @@
-let input = document.querySelector('input')
-let addBtn = document.getElementById("add")
+let input = document.querySelector("input");
+let addBtn = document.getElementById("add");
+let delBtn = document.querySelectorAll(".delete");
 
-addBtn.addEventListener("click", e => {
-    post_todos()
-})
-
-let todos = []
-
-async function get_todos() {
-    try {
-        let resp = await fetch("http://127.0.0.1:3000/todos");
-        todos = await resp.json();
-        console.log(todos);
-    } catch (error) {
-        console.error('Error fetching todos:', error);
-    }
-}
-
-async function post_todos() {
-    let todo_body = input.value;
-
-    console.log(todo_body);
-
-    let response = await fetch("http://127.0.0.1:3000/todos", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-            title: todo_body,
-        }),
-    });
-}
-
-async function delete_todos() {
-    let response = await fetch("http://127.0.0.1:3000/todos", {
-        method: "DELETE",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-            title: todo_body,
-        }),
-    });
-}
+let todos = [];
 
 get_todos();
 
-function update_todos(todo) {
-    let todo_body = document.createElement("p")
-    todo_body.innerText = todo.title
+addBtn.addEventListener("click", (e) => {
+  if (input.value != "") {
+    post_todo();
+  }
+});
 
+async function get_todos() {
+  try {
+    let resp = await fetch("http://127.0.0.1:3000/todos");
+    todos = await resp.json();
+    for (let i = 0; i < todos.length; i++) {
+      render_todo(todos[i]);
+    }
+  } catch (error) {
+    console.error("Error fetching todos:", error);
+  }
+}
 
+function render_todo(t) {
+  let todo = document.createElement("div");
+  todo.classList.add("todo");
+
+  let text = document.createElement("p");
+  text.innerText = t.title;
+
+  let dBtn = document.createElement("button");
+  dBtn.classList.add("delete");
+  dBtn.innerText = "Delete";
+
+  dBtn.addEventListener("click", () => {
+    delete_todo(t.id);
+  });
+
+  todo.appendChild(text);
+  todo.appendChild(dBtn);
+
+  document.querySelector("body").appendChild(todo);
+}
+
+async function post_todo() {
+  try {
+    let todo_body = input.value;
+
+    let resp = await fetch("http://127.0.0.1:3000/todos", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        title: todo_body,
+      }),
+    });
+    location.reload();
+  } catch (error) {
+    console.error("Error posting todo:", error);
+  }
+}
+
+async function delete_todo(id) {
+  try {
+    let resp = await fetch(`http://127.0.0.1:3000/todos/${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    location.reload();
+  } catch (error) {
+    console.error("Error deleting todo:", error);
+  }
 }
